a9cd12ab65f556b1ec76cc397a143d3a5f289804
anderconsu <anderconsu@gmail.com>
2023-06-28 13:46:59 +0200

    ingredients can be deleted now

--- js/ingList.mjs
+++ js/ingList.mjs
@@ -12,7 +12,17 @@ class ingList {
         this.lista.forEach((ing) => {
             let li = document.createElement("li");
             li.textContent = `${ing.cantidad}${ing.unidad} de ${ing.ingrediente}`;
+            let button = document.createElement("button");
+            button.textContent = "Eliminar";
+            button.addEventListener("click", () => {
+                console.log("Eliminar seleccionado")
+                this.lista.splice(this.lista.indexOf(ing), 1);
+                li.remove();
+
+            })
+            li.appendChild(button);
             lista.appendChild(li);
+
         });
     }
 }
